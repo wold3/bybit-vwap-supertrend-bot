7193aaa0b26f4b7a41ce078dcd9bcfba83455052
wold3 <114967598+wold3@users.noreply.github.com>
2026-07-06 03:35:17 +0900

    Update state.py

--- state.py
+++ state.py
@@ -3,13 +3,8 @@ import time
 trade_count = 0
 last_reset = time.time()
 
-positions = {
-    "BTCUSDT": {"active": False, "entry": 0},
-    "ETHUSDT": {"active": False, "entry": 0}
-}
-
-
 def can_trade():
+
     global trade_count, last_reset
 
     if time.time() - last_reset > 60:
@@ -21,7 +16,3 @@ def can_trade():
 
     trade_count += 1
     return True
-
-
-def get_position(symbol):
-    return positions[symbol]
