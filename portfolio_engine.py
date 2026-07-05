53f35d01c632e612004118a7433b4aab4f757490
wold3 <114967598+wold3@users.noreply.github.com>
2026-07-06 03:18:52 +0900

    Create portfolio_engine.py

---
+++ portfolio_engine.py
@@ -0,0 +1,22 @@
+class Portfolio:
+
+    def __init__(self):
+
+        self.strategies = {
+            "trend": {"capital": 400},
+            "range": {"capital": 300},
+            "safe": {"capital": 300}
+        }
+
+    def allocate(self, regime):
+
+        if regime == "TREND_UP":
+            return "trend"
+
+        if regime == "RANGE":
+            return "range"
+
+        return "safe"
+
+
+portfolio = Portfolio()
