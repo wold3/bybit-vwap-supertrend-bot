1d1666df4cb8ffe92e75d283b9a9c400be87386c
wold3 <114967598+wold3@users.noreply.github.com>
2026-07-07 02:02:33 +0900

    Update pnl_engine.py

--- execution/pnl_engine.py
+++ execution/pnl_engine.py
@@ -1,13 +1,13 @@
 from api.bybit_client import bybit
 
 
-class PnLEngine:
+class PnL:
 
     def get(self, symbol):
 
         res = bybit.position(symbol)
 
-        pnl = 0
+        pnl = 0.0
 
         for p in res.get("result", {}).get("list", []):
             pnl += float(p.get("unrealisedPnl", 0))
@@ -15,4 +15,4 @@ class PnLEngine:
         return pnl
 
 
-pnl_engine = PnLEngine()
+pnl_engine = PnL()
