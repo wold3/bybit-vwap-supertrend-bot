6b47de560b9b7584770a28cdb48ed521f68a3deb
wold3 <114967598+wold3@users.noreply.github.com>
2026-07-07 01:54:29 +0900

    Update bybit_client.py

--- api/bybit_client.py
+++ api/bybit_client.py
@@ -1,5 +1,4 @@
 import requests
-
 from config import BYBIT_TESTNET
 
 
@@ -7,16 +6,16 @@ class BybitClient:
 
     def __init__(self):
 
-        self.base_url = (
+        self.base = (
             "https://api-testnet.bybit.com"
             if BYBIT_TESTNET else
             "https://api.bybit.com"
         )
 
-    def place_order(self, symbol, side, qty):
+    def order(self, symbol, side, qty):
 
         return requests.post(
-            self.base_url + "/v5/order/create",
+            self.base + "/v5/order/create",
             json={
                 "category": "linear",
                 "symbol": symbol,
@@ -26,10 +25,10 @@ class BybitClient:
             }
         ).json()
 
-    def get_positions(self, symbol):
+    def position(self, symbol):
 
         return requests.get(
-            self.base_url + "/v5/position/list",
+            self.base + "/v5/position/list",
             params={
                 "category": "linear",
                 "symbol": symbol
@@ -37,4 +36,4 @@ class BybitClient:
         ).json()
 
 
-bybit_client = BybitClient()
+bybit = BybitClient()
