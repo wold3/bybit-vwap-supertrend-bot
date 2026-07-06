e1879f18df4784370498e804446dc32da93292af
wold3 <114967598+wold3@users.noreply.github.com>
2026-07-07 01:28:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-import logging
-import threading
 import time
+import threading
+import logging
 
 from api.websocket_client import ws_client
 from execution_worker import worker_loop
-
 from services.watchdog_service import watchdog
 from services.telegram_service import init_telegram
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 
 def init_system():
-
-    logger.info("SYSTEM INIT START")
-
     init_telegram("TOKEN", "CHAT_ID")
-
     watchdog.start()
-
-    logger.info("SYSTEM READY")
 
 
 if __name__ == "__main__":
@@ -31,7 +23,7 @@ if __name__ == "__main__":
 
     ws_client.start()
 
-    logger.info("TRADING ENGINE RUNNING")
+    print("TRADING ENGINE STARTED")
 
     while True:
         time.sleep(10)
