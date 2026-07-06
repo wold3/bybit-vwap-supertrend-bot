b14842845a5ad1bbf7d6d165124e79f5b471bb57
wold3 <114967598+wold3@users.noreply.github.com>
2026-07-07 01:13:18 +0900

    Create event_bus.py

---
+++ services/event_bus.py
@@ -0,0 +1,16 @@
+import queue
+import threading
+
+class EventBus:
+
+    def __init__(self):
+        self.q = queue.Queue()
+
+    def publish(self, event):
+        self.q.put(event)
+
+    def subscribe(self):
+        return self.q
+
+
+event_bus = EventBus()
