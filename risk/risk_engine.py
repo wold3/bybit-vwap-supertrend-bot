f6cee69cf75f08758d70dc795814f7933618fcf5
wold3 <114967598+wold3@users.noreply.github.com>
2026-07-07 01:55:43 +0900

    Update risk_engine.py

--- risk/risk_engine.py
+++ risk/risk_engine.py
@@ -1,48 +1,25 @@
-import datetime
-
-
 class RiskEngine:
 
     def __init__(self):
-
         self.pnl = 0
         self.trades = 0
         self.enabled = True
-        self.day = datetime.date.today()
 
-    def reset(self):
-
-        self.pnl = 0
-        self.trades = 0
-        self.enabled = True
-        self.day = datetime.date.today()
-
-    def check_reset(self):
-
-        if self.day != datetime.date.today():
-            self.reset()
-
-    def allow_trade(self):
-
-        self.check_reset()
-
-        if not self.enabled:
-            return False
+    def allow(self):
 
         if self.pnl <= -50:
             self.enabled = False
-            return False
 
         if self.trades >= 20:
             return False
 
-        return True
+        return self.enabled
 
     def update_pnl(self, pnl):
-        self.pnl += pnl
+        self.pnl = pnl
 
     def add_trade(self):
         self.trades += 1
 
 
-risk_engine = RiskEngine()
+risk = RiskEngine()
