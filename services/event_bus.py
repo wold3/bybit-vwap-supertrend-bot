077cd914ed38d096502a1dcb72343c2bb1ea889a
wold3 <114967598+wold3@users.noreply.github.com>
2026-07-07 01:54:13 +0900

    Refactor EventBus to use queue directly

--- services/event_bus.py
+++ services/event_bus.py
@@ -1,16 +1,3 @@
 import queue
 
-
-class EventBus:
-
-    def __init__(self):
-        self.q = queue.Queue()
-
-    def publish(self, event):
-        self.q.put(event)
-
-    def subscribe(self):
-        return self.q
-
-
-event_bus = EventBus()
+event_bus = queue.Queue()
