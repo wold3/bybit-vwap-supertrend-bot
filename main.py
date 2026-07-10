403b5f99f4717aa830356333c4700308553c0ef0
wold3 <114967598+wold3@users.noreply.github.com>
2026-07-11 01:38:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,32 @@ import signal
 import sys
 import time
 
+
 from app import TradingApp
+
 
 
 app = TradingApp()
 
 
 
+running = True
+
+
+
 def shutdown_handler(sig, frame):
+
+    global running
+
 
     print("[SYSTEM] SHUTDOWN")
 
+
+    running = False
+
+
     app.stop()
+
 
     sys.exit(0)
 
@@ -37,20 +51,33 @@ if __name__ == "__main__":
 
     try:
 
+
         app.start()
 
 
-        while True:
 
-            time.sleep(1)
+        while running:
+
+
+            # Health Check
+
+            if hasattr(app, "health_check"):
+
+                app.health_check()
+
+
+
+            time.sleep(5)
 
 
 
     except Exception as e:
 
+
         print(
             "[MAIN ERROR]",
             e
         )
+
 
         app.stop()
