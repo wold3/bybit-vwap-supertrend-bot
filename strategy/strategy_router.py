b16d497b5d152dc8b788c49562c62f444ffdebcb
wold3 <114967598+wold3@users.noreply.github.com>
2026-07-12 00:48:44 +0900

    Update strategy_router.py

--- strategy/strategy_router.py
+++ strategy/strategy_router.py
@@ -1,24 +1,22 @@
 from market.candle_builder import candle_builder
 from strategy.vwap import calculate_vwap
-from strategy.supertrend import supertrend
+from strategy.supertrend import calculate_supertrend
+
 from config import SYMBOL
+
+import pandas as pd
 
 
 def update_market_state(price, volume):
 
-    # 새 틱 반영
     candle_builder.update(
         SYMBOL,
         price,
         volume
     )
 
-    # 최신 캔들 조회
-    candles = candle_builder.get_candles(
-        SYMBOL
-    )
+    candles = candle_builder.get_candles(SYMBOL)
 
-    # 데이터 부족 시
     if len(candles) < 20:
         return {
             "price": price,
@@ -26,8 +24,18 @@ def update_market_state(price, volume):
             "signal": None
         }
 
-    vwap = calculate_vwap(candles)
-    signal = supertrend(candles)
+    df = pd.DataFrame(candles)
+
+    vwap = calculate_vwap(df)
+
+    trend = calculate_supertrend(df)
+
+    if trend is True:
+        signal = "Buy"
+    elif trend is False:
+        signal = "Sell"
+    else:
+        signal = None
 
     return {
         "price": price,
